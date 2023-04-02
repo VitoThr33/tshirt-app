20b89b251f6cf7ec352b40a794afa0d281c4c0b7
Ryan McGowan <vitomaserati@gmail.com>
2023-04-01 20:53:04 -0700

    add side tab on customizer page

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,13 +1,13 @@
-import React, {useRef} from 'react'
-import { useFrame } from '@react-three/fiber'
-import {easing} from 'maath'
-import { useSnapshot } from 'valtio'
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
 
-import state from '../store'
+import state from '../store';
 
-const CameraRig = ({children}) => {
+const CameraRig = ({ children }) => {
   const group = useRef();
-  const snap= useSnapshot(state)
+  const snap = useSnapshot(state);
 
   useFrame((state, delta) => {
     const isBreakpoint = window.innerWidth <= 1260;
@@ -26,20 +26,17 @@ const CameraRig = ({children}) => {
     // set model camera position
     easing.damp3(state.camera.position, targetPosition, 0.25, delta)
 
-    //set the model rotation
+    // set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
       0.25,
       delta
     )
-   })
+  })
 
-  
 
-  return (
-    <group ref={group}>{children}</group>
-  )
+  return <group ref={group}>{children}</group>
 }
 
 export default CameraRig
